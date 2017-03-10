81d47ba9b2f5214d5739b2503cd59b0cb5b1a523
Christian Cataldo <christian.cataldo@itcollege.ee>
2017-03-10 16:37:41 +0200

    Add login page and improve main page

--- main.py
+++ main.py
@@ -1,41 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
+# print(r"http://localhost:5000/")    # r=print all characters within ""
 
-#print(r"http://localhost:5000/")	#r=print all characters within ""
+
 @app.route('/')
-def index():
-	return "Method used: %s" % request.method
+def home():
+    return render_template("index.html")
+    
+''' 
+@app.route("/<user>")
 
-@app.route('/method', methods=['GET', 'POST'])
-def method():
-	if request.method == 'POST':
-		return "You are now using POST"
-	else:
-		return "You are now using GET"
+def index(user=None):
+    # return "Method used: %s" % request.method
+    return render_template("user.html", user=user) 
+'''
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Wrong username/password, please try again'
+        else:
+            return redirect(url_for('home'))    
+    return render_template("login.html", error=error)
+
+
+@app.route('/shop')
+def shop():
+    food = ["Beer", "Pizza", "Kethcup"]
+    return render_template("shop.html", food=food) 
 
 
 @app.route('/page01')
 def page01():
-	return "<h2>This is page 2</h2>"
+    return "<h2>This is page 2</h2>"
 
 
 '''@app.route('/profile/<username>')
 def profile(username):
-	return "Hello %s" % username
+    return "Hello %s" % username
+'''
 '''
 @app.route('/profile/<name>')
 def profile(name):
-	return render_template("profile.html", name=name)
+    return render_template("profile.html", name=name)
+'''
 
-
-
-
+'''
 @app.route('/post/<int:post_id>')
 def post(post_id):
-	return "The Post_ID is %s" % post_id
-
+    return "The Post_ID is %s" % post_id
+'''
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
+    # app.run()
